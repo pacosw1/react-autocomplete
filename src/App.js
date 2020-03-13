6accaec8eaf258270370f9d2567e17becaf12200
Paco <pacoswilliams@gmail.com>
2020-03-12 21:04:51 -0600

    api calls

--- src/App.js
+++ src/App.js
@@ -14,13 +14,48 @@ class App extends Component {
       { name: "Alicia keys", age: 25, position: "Librarian" },
       { name: "Alexa Dot", age: 30, position: "teacher" },
       { name: "Bob Squarepants", age: 20, position: "secretary" }
-    ]
+    ],
+    keyword: "",
+    results: []
+  };
+
+  matchName = (name, keyword) => {
+    var keyLen = keyword.length;
+    name = name.toLowerCase().substring(0, keyLen);
+    if (keyword == "") return false;
+    return name == keyword.toLowerCase();
+  };
+
+  onSearch = async text => {
+    if (text != "") {
+      var stockData, data;
+      try {
+        stockData = await fetch(
+          `https://financialmodelingprep.com/api/v3/search?query=${text}&limit=10&exchange=NASDAQ`
+        );
+        data = await stockData.json();
+      } catch (err) {
+        console.log(err.message);
+      }
+
+      this.setState({ results: data });
+    } else this.setState({ results: [] });
+  };
+
+  updateField = (field, value, update = true) => {
+    if (update) this.onSearch(value);
+    this.setState({ [field]: value });
   };
   render() {
-    let { data } = this.state;
+    let { results, keyword } = this.state;
+
     return (
       <div className="App">
-        <SearchBar data={data} />
+        <SearchBar
+          results={results}
+          keyword={keyword}
+          updateField={this.updateField}
+        />
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,83 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
 import "./css/SearchBar.css";
 
-class SearchBar extends Component {
-  state = {
-    results: [],
-    keyword: ""
+const SearchBar = ({ results, keyword, updateField }) => {
+  //renders our results using the SearchPreview component
+  var updateText = text => {
+    updateField("keyword", text, false);
+    updateField("results", []);
   };
 
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-  }
-
-  //will match keyword with a potenital item in our data array
-  matchName = (name, keyword) => {
-    var keyLen = keyword.length;
-    name = name.toLowerCase().substring(0, keyLen);
-    if (keyword == "") return false;
-    return name == keyword.toLowerCase();
+  var cancelSearch = () => {
+    updateField("keyword", "");
   };
 
-  updateText = text => {
-    this.setState({ keyword: text, visible: false });
-  };
-  onSearch = text => {
-    var visible = true;
-    //hide results div if input value is empty
-    let { data } = this.props;
-    //check to see if we found a match, if so, add it to results
-    var results = data.filter(item => true == this.matchName(item.name, text));
-    //if we didnt find any match, hide results div
-    if (results.length < 1 || this.text == "") visible = false;
-
-    //update state changes
-    this.setState({ visible, results, keyword: text });
-  };
-
-  cancelSearch = () => {
-    this.setState({ visible: false, results: [], keyword: "" });
-  };
-
-  render() {
-    //renders our results using the SearchPreview component
-    var results = this.state.results.map(({ position, name, age }, index) => {
-      return (
-        <SearchPreview
-          key={index}
-          updateText={this.updateText}
-          index={index}
-          position={position}
-          name={name}
-          age={age}
-        />
-      );
-    });
+  var renderResults = results.map(({ position, name, age }, index) => {
     return (
-      <div className="auto">
-        <button
-          onClick={() => this.cancelSearch()}
-          className={`cancel-btn ${
-            this.state.keyword.length > 0 ? "active" : "inactive"
-          }`}
-        >
-          x
-        </button>
-        <input
-          className="search-bar"
-          placeholder="Search"
-          value={this.state.keyword}
-          onChange={e => this.onSearch(e.target.value)}
-        />
-
-        {this.state.visible ? (
-          <div className="search-results">{results}</div>
-        ) : null}
-      </div>
+      <SearchPreview
+        key={index}
+        updateText={updateText}
+        index={index}
+        position={position}
+        name={name}
+        age={age}
+      />
     );
-  }
-}
+  });
+  return (
+    <div className="auto">
+      <button
+        onClick={() => cancelSearch()}
+        className={`cancel-btn ${keyword.length > 0 ? "active" : "inactive"}`}
+      >
+        x
+      </button>
+      <input
+        className="search-bar"
+        placeholder="Search"
+        value={keyword}
+        onChange={e => updateField("keyword", e.target.value)}
+      />
+
+      {results.length > 0 ? (
+        <div className="search-results">{renderResults}</div>
+      ) : null}
+    </div>
+  );
+};
 
 //stateless component to render preview results
 const SearchPreview = ({ age, name, position, index, updateText }) => {
@@ -90,10 +57,10 @@ const SearchPreview = ({ age, name, position, index, updateText }) => {
         <p className="name">{name}</p>
         <p className="sub-header">{position}</p>
       </div>
-      <div className="second">
+      {/* <div className="second">
         <p className="age">{age}</p>
         <p className="sub-header">age</p>
-      </div>
+      </div> */}
     </div>
   );
 };
